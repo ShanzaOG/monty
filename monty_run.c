2043c451ae9a4b03538fad35a7ccf10f56aa6f03
ShanzaOG <allanshanza@gmail.com>
2023-05-26 10:22:17 -0700

    unpush all files Revert "changes"
    This reverts commit 04d2c380f2272c623b9460a0395f02fcee2541a4.

--- monty_run.c
+++ monty_run.c
@@ -1,46 +1,64 @@
 #include "monty.h"
-
+#include <stdio.h>
 /**
  * monty_run - A function that runs a Monty bytecodes script.
  * @fd: File descriptor for an open Monty bytecodes script.
  * Return: EXIT_SUCCESS on success, or EXIT_FAILURE on error.
  */
+
+#define MAX_LINE_LENGTH 256
+
 int monty_run(FILE *fd)
 {
-	stack_t *stack = NULL;
-	char *line = NULL, **token = NULL, delim[] = " \n\t\a\b";
-	size_t len = 0, mode = 1;
-	unsigned int line_num = 0, exit_status = EXIT_SUCCESS;
+    stack_t *stack = NULL;
+    char line[MAX_LINE_LENGTH];
+    char **token = NULL;
+    char delim[] = " \n\t\a\b";
+    size_t line_num = 0;
+    int exit_status = EXIT_SUCCESS;
 
-	while (getline(&line, &len, fd) != -1)
-	{
-		line_num++;
-		if (empty_line(line, delim))
-			continue;
-		token = tokening(line, delim);
-		if (token[0][0] == '#' || strcmp(token[0], "nop") == 0)
-		{
-			free(token);
-			continue;
-		}
-		else if (strcmp(token[0], "stack") == 0)
-			mode = 1;
-		else if (strcmp(token[0], "queue") == 0)
-			mode = 0;
-		else if (strcmp(token[0], "push") == 0 && mode == 0)
-			exit_status = monty_pushq(&stack, token, line_num);
-		else if (strcmp(token[0], "push") == 0 && mode == 1)
-			exit_status = monty_push(&stack, token, line_num);
-		else
-			exit_status = execute(token, &stack, line_num);
-		free(token);
-		if (exit_status == EXIT_FAILURE)
-			break;
-	}
-	free_stack(&stack);
-	free(line);
-	return (exit_status);
+    while (fgets(line, sizeof(line), fd) != NULL)
+    {
+        line_num++;
+        if (empty_line(line, delim))
+            continue;
+
+        token = tokening(line, delim);
+
+        if (token[0][0] == '#' || strcmp(token[0], "nop") == 0)
+        {
+            free(token);
+            continue;
+        }
+        else if (strcmp(token[0], "stack") == 0)
+        {
+            free(token);
+            continue;
+        }
+        else if (strcmp(token[0], "queue") == 0)
+        {
+            free(token);
+            continue;
+        }
+        else if (strcmp(token[0], "push") == 0)
+        {
+            free(token);
+            continue;
+        }
+
+        exit_status = execute(token, &stack, line_num);
+
+        free(token);
+
+        if (exit_status == EXIT_FAILURE)
+            break;
+    }
+
+    free_stack(&stack);
+
+    return exit_status;
 }
+
 
 /**
  * tokening - A function that split the line.
